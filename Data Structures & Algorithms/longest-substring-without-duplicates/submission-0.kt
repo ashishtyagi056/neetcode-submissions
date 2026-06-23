9264fc43bf69db24f7e145a6dccd3df745f04f97
Ashish Tyagi <tyagiashish056@gmail.com>
2026-06-23 16:15:38 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var left = 0
+        var maxLength = 0
+        val set = HashSet<Char>()
+        for (right in s.indices) {
+            while (set.contains(s[right])) {
+                set.remove(s[left])
+                left++
+            }
+            set.add(s[right])
+            maxLength = maxOf(maxLength, right-left+1)
+        }
+        return maxLength
+    }
+}
