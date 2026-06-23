7853062af84605b215edd8e893ca93e0438f3ef2
Ashish Tyagi <tyagiashish056@gmail.com>
2026-06-23 17:53:47 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        var missing = nums.size
+        for (i in nums.indices) {
+            missing = missing xor i xor nums[i]
+        }
+        return missing
+    }
+}
